6831339023e535ef64b3b401df4455c04d397a63
BangL <henno.rickowski@gmail.com>
2017-09-10 20:13:56 +0200

    some refactoring, implemented temporary fix for in chat until its fixed in btl

--- re.lua
+++ re.lua
@@ -1,43 +1,42 @@
--- The code is the documentation and many quotes on the internet are attributed incorrectly. (Mahatma "The Condor" Gandhi)
-if game_state_machine then
-	if game_state_machine:current_state_name() == "menu_main" then
+
+-- this is a temporary fix, which can be removed as soon as the corresponding pull request is merged into the blt.
+local input_focus =	(managers.menu_component and managers.menu_component:input_focut_game_chat_gui()) -- 'focut' is not a typo on our side.
+
+if not input_focus and game_state_machine then
+	local state = game_state_machine:current_state_name()
+	if state == "menu_main" then
 		if Network:multiplayer() then
-			local callback_handler = managers.menu:get_menu("menu_main").callback_handler
-			local dialog_data = {
-				title = managers.localization:text("dialog_warning_title"),
-				text = managers.localization:text("dialog_are_you_sure_you_want_leave"),
-				id = "leave_lobby_RELUA"
+			local title = managers.localization:text("dialog_warning_title")
+			local text = managers.localization:text("dialog_are_you_sure_you_want_leave")
+			local options = {
+				{
+					text = managers.localization:text("dialog_yes"),
+					callback = MenuCallbackHandler._relua_lobby
+				},
+				{
+					text = managers.localization:text("dialog_no"),
+					is_cancel_button = true
+				}
 			}
-			local yes_button = {
-				text = managers.localization:text("dialog_yes"),
-				callback_func = callback(callback_handler, callback_handler, "_dialog_leave_lobby_yes_RELUA")
-			}
-			local no_button = {
-				text = managers.localization:text("dialog_no"),
-				callback_func = callback(callback_handler, callback_handler, "_dialog_leave_lobby_no"),
-				cancel_button = true
-			}
-			dialog_data.button_list = {
-				yes_button,
-				no_button
-			}
-			managers.system_menu:show(dialog_data)
+			QuickMenu:new(title, text, options, true)
 		else
-			MenuCallbackHandler:_RELUA()
+			MenuCallbackHandler:_relua_start_menu()
 		end
-	elseif game_state_machine:current_state_name() == "ingame_waiting_for_players" or game_state_machine:current_state_name() == "ingame_mask_off" or game_state_machine:current_state_name() == "ingame_standard" then
-		if Network:is_server() and not managers.job:is_current_job_professional() and not in_chat then
+	elseif state == "ingame_waiting_for_players" or state == "ingame_mask_off" or state == "ingame_standard" then
+		if Network:is_server() then
 			local all_synced = true
-			for k,v in pairs(managers.network:session():peers()) do
+			for _,v in pairs(managers.network:session():peers()) do
 				if not v:synched() then
 					all_synced = false
 				end
 			end
-			if all_synced == true then
+			if all_synced then
 				managers.game_play_central:restart_the_game()
 			else
 				managers.chat:_receive_message(1, "[ReLUA]", "Cannot restart, a player may be loading.", Color.red)
 			end
+		--else
+			-- TODO: client reconnect
 		end
 	end
 end
